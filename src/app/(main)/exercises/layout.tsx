07ff97d155ef3c86b11db6693b2e1574e671e7d3
Precious Olusola <preciousolusola16@gmail.com>
2025-05-26 11:51:46 +0100

    Added metadata for exercises page

--- src/app/(main)/exercises/layout.tsx
+++ src/app/(main)/exercises/layout.tsx
@@ -1,5 +1,15 @@
 import "katex/dist/katex.min.css";
 import { ExerciseProvider } from "@/lib/context/ExerciseContext";
+import { Metadata } from "next";
+import { default_metadata } from "@/data/framework";
+
+export const metadata: Metadata = {
+    title: "Exercises - Your exercises",
+    description:
+        "Master probability with Probify. Gamified lessons, leaderboards, and interactive challenges designed for students aged 13–16.",
+    ...default_metadata,
+};
+
 
 export default function ExerciseLayout({
     children,
--- src/data/framework.ts
+++ src/data/framework.ts
@@ -1,4 +1,40 @@
+import { APP_LIVE_URL } from "./links";
 
 export const headerMenu = [];
 export const MIN_EXERCISE_QUESTIONS = 5
 export const MIN_EXERCISE_DURATION = 5
+
+export const default_metadata = {
+    keywords: [
+        "probability",
+        "math learning",
+        "e-learning",
+        "probability app",
+        "interactive math",
+        "learn probability",
+        "Probify",
+    ],
+    openGraph: {
+        title: "Probify – Learn Probability the Smart Way",
+        description:
+            "Master probability with Probify. Gamified lessons, leaderboards, and interactive challenges designed for students aged 13–16.",
+        url: APP_LIVE_URL, // Change this to your real domain
+        siteName: "Probify",
+        images: [
+            {
+                url: "/og/banner.png",
+                width: 1200,
+                height: 630,
+                alt: "Probify Home Preview",
+            },
+        ],
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Probify – Learn Probability the Smart Way",
+        description:
+            "Gamified lessons and smart tools for mastering probability.",
+        images: ["/og/banner.png"],
+    },
+}
